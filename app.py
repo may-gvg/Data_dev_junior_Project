3507768e8d8095530c7b87a8a1163cd4314ac68b
may <83417030+may-gvg@users.noreply.github.com>
2021-09-14 14:29:07 +0200

    app route update, sciaganie danych

--- app.py
+++ app.py
@@ -1,125 +1,161 @@
-
-import numpy as np
-import pandas as pd
-
-# Numpy code:
-
-a = np.array([1,3,5,7])
-# array([1, 3, 5, 7])
-b = np.arange(4)
-# array([0, 1, 2, 3])
-np.arange(2,10,1)
-# array([2, 3, 4, 5, 6, 7, 8, 9])
-np.linspace(0,10,6)
-# array([ 0.,  2.,  4.,  6.,  8., 10.])
-
-c = np.array([[1,2,3],[4,5,6]])
-# array([[1, 2, 3],
-#       [4, 5, 6]])
-
-d = np.zeros((2,3))
-# array([[0., 0., 0.],
-#       [0., 0., 0.]])
-
-d = np.ones((2,3))
-# array([[1., 1., 1.],
-#       [1., 1., 1.]])
-
-np.random.random((2,3))
-# array([[0.16868203, 0.31090109, 0.7210469 ],
-#       [0.0750029 , 0.08401428, 0.96992496]])
+import csv
+import requests
+from flask import Flask, render_template, redirect, url_for, request, flash, abort, make_response
+from flask import send_file
+app = Flask(__name__)
 
 
-a+b
-# array([ 1,  4,  7, 10])
-
-c+d
-# array([[2., 3., 4.],
-#       [5., 6., 7.]])
-
-c**2
-# array([[ 1,  4,  9],
-#       [16, 25, 36]])
-
-c.shape
-# (2, 3)
-
-c.T
-# array([[1, 4],
-#       [2, 5],
-#       [3, 6]])
-c > 3
-# array([[False, False, False],
-#       [ True,  True,  True]])
-
-a[2:]
-# array([5, 7])
-c[:1]
-# array([[1, 2, 3]])
-c[:1,1:]
-# array([[2, 3]])
-for x in c:
-    print (x)
-    for y in x:
-        print(y)
-# [4 5 6]
-# 1
-# 2
-# 3
-# [4 5 6]
-# 4
-# 5
-# 6
-for x in c.flat:
-    print(x)
-# 1
-# 2
-# 3
-# 4
-# 5
-# 6
-[x for x in c if x[1] < 3]
-# [array([1, 2, 3])]
+def read_csv():
+    csvlist = {}
+    with open('static/analiza/output.csv', newline='', encoding="UTF-8") as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+        for row in spamreader:
+            csvlist[row[1]] = row[3]
+        return csvlist
 
 
-x = np.random.random((3,5))
-x
-# array([[0.43391468, 0.73430824, 0.92759808, 0.23966575, 0.16112573],
-#       [0.85501039, 0.67383523, 0.45245038, 0.26001489, 0.86884395],
-#       [0.17991555, 0.96628615, 0.22680872, 0.92979024, 0.52796391]])
-print(x.sum())
-print(x.min())
-print(x.max())
-print(x.mean())
-# 8.437531894577779
-# 0.1611257283125176
-# 0.9662861490415007
-# 0.5625021263051853
-
-one_dim = np.array([1,2,3,4])
-
-two_dim = np.array([[100, 200, 50, 400], [50, 0, 0, 100], [350, 100, 50, 200]])
-
-print("len: " + str(len(two_dim)))      # len tu nie rozumiem
-print("shape: " + str(two_dim.shape))  # rozmiar, kształt tablicy
-print("ndim: " + str(two_dim.ndim))   #ilość wymiarów
-print("size: " + str(two_dim.size))  #ilość elementów
-print("bajt-size: " + str(two_dim.itemsize))
+def sciagnij_waluty():
+    try:
+        response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
+        data_from_json = response.json()
+        print(type(data_from_json))
+        with open("static/analiza/output.csv", 'w', encoding='utf-8', newline='') as csv_file:
+            writer = csv.writer(csv_file, delimiter=';')
+            rates = data_from_json[0]['rates']
+            for rate in rates:
+                currency = rate['currency']
+                code = rate['code']
+                bid = rate['bid']
+                ask = rate['ask']
+                print(currency, code, bid, ask)
+                writer.writerow([currency, code, bid, ask])
+            csv_file.close()
+    except:
+        print("Błąd")
+        return
 
 
-# numpy jaka jest roznica pomiedzy dot/matmul/mul, jakie sa przewagi , do czego sie uzywa, ja zmienic ksztalt
-#( reshape/view ), jak utworzyc losowe tablice o rozmiarze jakims, jak same jedynki utworzyc (zeros/once),
-# generacja randomowych liczb, np.where(...),
+sciagnij_waluty()
 
-# 1. stwórz array na 2 sposoby w kilku wymiarach (3,4 tablice),
-# stwórz jedną losową z użyciem where,
-# użyj np.linspace(0,10,6),
-# stwórz zerowa i jedynkową tablicę
-# 2. pokaż ilość wymiarów i rozmiar tablicy
-# 3.  policz ilość elemntów i ich sumę
-# 4. pokaż indeksem elementy tablicy
-# 5 iteruj i wydrukuj tablicę
-# 6. zmień rozmiar tablicy
-# 7. stwórz matrix, pokaż indeksy po matriksie
-# 8 zmień wymiar i rozmiar tablicy (reshape, view)
-# 9. numpy jaka jest roznica pomiedzy dot/matmul/mul - zastosuj
+
+@app.route('/waluty', methods=['GET', 'POST'])
+def waluty():
+    args = ""
+    data3 = []
+    with open('static/analiza/output.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        for row in reader:
+            data3.append(row)
+    data = [['currency', 'code', 'bid', 'ask']]
+
+    waluty = read_csv()
+    options = []
+    for waluta in waluty:
+        options.append(waluta)
+    print(options)
+
+    if request.method == 'GET':
+        print("We received GET")
+        return render_template("waluty.html", options=options, args=args, data3=data3, data=data)
+        #return render_template("waluty.html", options=options)
+    elif request.method == 'POST':
+        print("We received POST")
+        print(request.form)
+        currency = request.form.get('zczego')
+        money = float(request.form.get('kwota'))
+        waluty = read_csv()
+        for waluta in waluty:
+            if waluta == currency:
+                wartosc = float(waluty[waluta])
+                print("Wartosc: " + str(wartosc))
+        przelicz = ""
+        print("Kwota: " + str(money))
+        print("currenty: " + currency)
+        przelicz = str(round(money * wartosc, 3))
+        print("przelicz: " + przelicz)
+        return render_template("waluty.html", wynik=przelicz, options=options, args=args, data3=data3, data=data)
+
+
+@app.route('/')
+def homepage():
+    args = ""
+    data = []
+    with open('static/analiza/analiza.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        c = 1
+        for row in reader:
+            if c == 1:
+                c += 1
+                continue
+            data.append(row)
+    data2 = []
+    with open('static/analiza/analiza.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            data2.append(row)
+            break
+    return render_template("homepage.html", args=args, data=data, data2=data2)
+
+
+
+
+@app.route('/sciagnij/')
+def plot_csv():
+    return send_file('static/analiza/output.csv',
+                     mimetype='text/csv',
+                     attachment_filename='output.csv',
+                     as_attachment=True)
+
+
+
+@app.route('/numpy')
+def numpy_page():
+    args = ""
+    return render_template("numpy.html", args=args)
+
+
+@app.route('/pandas')
+def pandas_page():
+    args = ""
+    return render_template("pandas.html", args=args)
+
+
+@app.route('/wykresy')
+def wykresy_page():
+    args = ""
+    return render_template("wykresy.html", args=args)
+
+
+@app.route('/analiza')
+def analiza_page():
+    args = ""
+    return render_template("analiza.html", args=args)
+
+
+@app.route('/flask')
+def flask_page():
+    args = ""
+    return render_template("flask.html", args=args)
+
+
+@app.route('/pytest')
+def pytest_page():
+    args = ""
+    return render_template("pytest.html", args=args)
+
+
+@app.route('/analiza2')
+def analiza2_page():
+    args = ""
+    return render_template("analiza2.html", args=args)
+
+
+@app.route('/analiza3')
+def analiza3_page():
+    args = ""
+    return render_template("analiza3.html", args=args)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
