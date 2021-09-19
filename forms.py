8411a88c36fb7ce87403342ffabed986b56f8ea9
Maciej Gubała <mac.gub@gmail.com>
2021-09-19 23:26:07 +0200

    make it better

---
+++ forms.py
@@ -0,0 +1,9 @@
+
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField
+from wtforms import SubmitField
+
+
+class MyForm(FlaskForm):
+    file = FileField('File')
+    submit = SubmitField('Submit')
