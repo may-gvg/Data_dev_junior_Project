1d23a0b1ec97a76f55e24d9b6ddf25d3b2686907
may <83417030+may-gvg@users.noreply.github.com>
2021-10-14 20:05:15 +0200

    v4

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 import requests
 import seaborn as sns
 import urllib3
-from flask import Flask, render_template, redirect, request, flash, send_file
+from flask import Flask, render_template, redirect, request, flash, send_file, session
 from json2html import *
 from werkzeug.utils import secure_filename
 from wtforms import StringField, Form, validators, SubmitField, SelectField
@@ -23,7 +23,6 @@ app.config.from_object(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
-
 Session(app)
 
 
@@ -96,28 +95,35 @@ def waluty():
         return render_template("waluty.html", wynik=przelicz, options=options, args=args, data3=data3, data=data)
 
 
+def nice_loader(loader):
+    global df2, data, data1, data2, data3, data4, data5
+    if loader == 'csv':
+        df2 = pd.read_csv('static/analiza/analiza.csv')
+    if loader == 'xls':
+        df2 = pd.read_excel('static/analiza/analiza.xls')
+    if loader == 'db':
+        cnx = sqlite3.connect('static/analiza/analiza.db')
+        df2 = pd.read_sql_query("SELECT * FROM courthouse_security_logs", cnx)
+    data = df2.iloc[:, 0].value_counts().sort_values(ascending=False).head(50)
+    data1 = df2.iloc[:, 1].value_counts().sort_values(ascending=False).head(50)
+    data2 = df2.iloc[:, 2].value_counts().sort_values(ascending=False).head(50)
+    data3 = df2.iloc[:, 3].value_counts().sort_values(ascending=False).head(50)
+    data4 = df2.iloc[:, 4].value_counts().sort_values(ascending=False).head(50)
+    data5 = df2.iloc[:, 5].value_counts().sort_values(ascending=False).head(50)
+
+
 @app.route('/')
 def homepage():
     global df2
-
-
     loader = request.args.get('loader', "none")
-
-    print("loader: " + loader)
-
     if loader == "none":
-        loader = 'csv'
-
-    if loader == 'csv':
-        df2 = pd.read_csv('static/analiza/analiza.csv')
-
-    if loader == 'xls':
-        print("loader: xls")
-        df2 = pd.read_excel('static/analiza/analiza.xls')
-
-    if loader == 'db':
-        cnx = sqlite3.connect('static/analiza/analiza.db')
-        df2 = pd.read_sql_query("SELECT * FROM flights", cnx)
+        loader2 = session.get('loader')
+        if not loader2:
+            loader = 'csv'
+        else:
+            loader = loader2
+    session['loader'] = loader
+    nice_loader(loader)
 
     args = ""
     data = []
@@ -133,6 +139,7 @@ def homepage():
         bar_data.append(wartosc)
         area_labels.append(waluta)
         bar_labels.append(waluta)
+
     with open('static/analiza/analiza.csv', newline='') as csvfile:
         reader = csv.reader(csvfile)
         c = 1
@@ -141,18 +148,21 @@ def homepage():
                 c += 1
                 continue
             data.append(row)
+
     data2 = []
     with open('static/analiza/analiza.csv', newline='') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             data2.append(row)
             break
+
     data3 = []
     for index, row in df2.iterrows():
         l = []
         for i in range(0, row.size):
             l.append(row[i])
         data3.append(l)
+
     data4 = []
     for c in df2.keys():
         data4.append(c)
@@ -162,90 +172,32 @@ def homepage():
                            bar_labels=bar_labels.__str__())
 
 
-@app.route('/csvloader')
-def csv_loader():
-    df2 = pd.read_csv('static/analiza/analiza.csv')
-    data = df2.iloc[:, 0].value_counts()
-    data1 = df2.iloc[:, 1].value_counts()
-    data2 = df2.iloc[:, 2].value_counts()
-    data3 = df2.iloc[:, 3].value_counts()
-    data4 = df2.iloc[:, 4].value_counts()
-    data5 = df2.iloc[:, 5].value_counts()
-
-    args = ""
-    return render_template("analiza2.html", args=args)
-
-
-@app.route('/xlsloader')
-def xls_loader():
-    df2 = pd.read_excel('static/analiza/analiza.xls')
-    data = df2.iloc[:, 5].value_counts()
-    data1 = df2.iloc[:, 6].value_counts()
-    data2 = df2.iloc[:, 7].value_counts()
-    data3 = df2.iloc[:, 8].value_counts()
-    data4 = df2.iloc[:, 9].value_counts()
-    data5 = df2.iloc[:, 10].value_counts()
-
-    args = ""
-    return render_template("analiza2.html", args=args)
-
-
-@app.route('/dbloader')
-def db_loader():
-    cnx = sqlite3.connect('static/analiza/analiza.db')
-    df2 = pd.read_sql_query("SELECT * FROM flights", cnx)
-    # wybór tabeli
-    data = df2.iloc[:, 5].value_counts()
-    data1 = df2.iloc[:, 6].value_counts()
-    data2 = df2.iloc[:, 7].value_counts()
-    data3 = df2.iloc[:, 8].value_counts()
-    data4 = df2.iloc[:, 9].value_counts()
-    data5 = df2.iloc[:, 10].value_counts()
-
-    args = ""
-    return render_template("analiza2.html", args=args)
-
-
-df2 = pd.read_csv('static/analiza/analiza.csv')
-
-
-# df2 = pd.read_excel('static/analiza/analiza.xls')
-
-# df2 = pd.read_sql_query("SELECT * FROM flights", cnx)
-# wybór tabeli
-
-
-# value counts data butony
-data = df2.iloc[:, 0].value_counts().sort_values(ascending=False).head(50)
-data1 = df2.iloc[:, 1].value_counts().sort_values(ascending=False).head(66)
-data2 = df2.iloc[:, 2].value_counts().sort_values(ascending=False).head(66)
-data3 = df2.iloc[:, 3].value_counts().sort_values(ascending=False).head(66)
-data4 = df2.iloc[:, 4].value_counts().sort_values(ascending=False).head(66)
-data5 = df2.iloc[:, 5].value_counts().sort_values(ascending=False).head(66)
-
-# describe data
-x = df2.describe()
-
 cmap = plt.get_cmap("Set2")
 colors = cmap(np.array([1, 2, 3, 4, 5, 6, 7]))
 
 
 @app.route('/analysis')
 def analysis():
-    return render_template("analysis.html", data7=x.to_html())
+    global df2
+    loader = session.get('loader')
+    if not loader:
+        loader = 'csv'
+    nice_loader(loader)
+    x = df2.describe()
+    z = df2.median(numeric_only=True)
+    z = pd.DataFrame(z)
+    y = df2.mode().head(3)
+    return render_template("analysis.html", data7=x.to_html(), data8=y.to_html(), data9=z.to_html())
 
 
 # kolumna 1
-
 def do_plot():
     # bar
     # Loading
-
+    global df2, data
     plt.figure(figsize=(12, 6))
     plt.title('col 1 value count')
     sns.barplot(x=data.index, y=data, palette='Set2')
-
-    # here is the trick save your figure into a bytes object and you can afterwards expose it via flas
     bytes_image = io.BytesIO()
     plt.savefig(bytes_image, format='png')
     bytes_image.seek(0)
@@ -255,12 +207,10 @@ def do_plot():
 def do_plot1():
     # line
     # Loading
-
+    global df2, data
     plt.figure(figsize=(12, 6))
     plt.title('col 1 value count')
     sns.lineplot(x=data.index, y=data, palette='Set2')
-
-    # here is the trick save your figure into a bytes object and you can afterwards expose it via flas
     bytes_image = io.BytesIO()
     plt.savefig(bytes_image, format='png')
     bytes_image.seek(0)
@@ -270,13 +220,11 @@ def do_plot1():
 def do_plot2():
     # kołowy
     # Loading
-
+    global df2, data
     plt.figure(figsize=(12, 6))
     plt.title('col 1 value count')
     plt.pie(data, labels=data.index, colors=colors, autopct='%1.1f%%', pctdistance=1.1, labeldistance=1.2,
             startangle=150, shadow=True)
-
-    # here is the trick save your figure into a bytes object and you can afterwards expose it via flas
     bytes_image = io.BytesIO()
     plt.savefig(bytes_image, format='png')
     bytes_image.seek(0)
@@ -288,12 +236,10 @@ def do_plot2():
 def do_plot3():
     # bar
     # Loading
-
+    global df2, data1
     plt.figure(figsize=(12, 6))
     plt.title('col 2 value count')
     sns.barplot(x=data1.index, y=data1, palette='Set2')
-
-    # here is the trick save your figure into a bytes object and you can afterwards expose it via flas
     bytes_image = io.BytesIO()
     plt.savefig(bytes_image, format='png')
     bytes_image.seek(0)
@@ -303,12 +249,10 @@ def do_plot3():
 def do_plot4():
     # line
     # Loading
-
+    global df2, data1
     plt.figure(figsize=(12, 6))
     plt.title('col 2 value count')
     sns.lineplot(x=data1.index, y=data1, palette='Set2')
-
-    # here is the trick save your figure into a bytes object and you can afterwards expose it via flas
     bytes_image = io.BytesIO()
     plt.savefig(bytes_image, format='png')
     bytes_image.seek(0)
@@ -318,13 +262,11 @@ def do_plot4():
 def do_plot5():
     # kołowy
     # Loading
-
+    global df2, data1
     plt.figure(figsize=(12, 6))
     plt.title('col 2 value count')
     plt.pie(data1, labels=data1.index, colors=colors, autopct='%1.1f%%', pctdistance=1.1, labeldistance=1.2,
             startangle=150, shadow=True)
-
-    # here is the trick save your figure into a bytes object and you can afterwards expose it via flas
     bytes_image = io.BytesIO()
     plt.savefig(bytes_image, format='png')
     bytes_image.seek(0)
@@ -337,12 +279,10 @@ def do_plot5():
 def do_plot6():
     # bar
     # Loading
-
+    global df2, data2
     plt.figure(figsize=(12, 6))
     plt.title('col 3 value count')
     sns.barplot(x=data2.index, y=data2, palette='Set2')
-
-    # here is the trick save your figure into a bytes object and you can afterwards expose it via flask
     bytes_image = io.BytesIO()
     plt.savefig(bytes_image, format='png')
     bytes_image.seek(0)
@@ -352,12 +292,10 @@ def do_plot6():
 def do_plot7():
     # line
     # Loading
-
+    global df2, data2
     plt.figure(figsize=(12, 6))
     plt.title('col 3 value count')
     sns.lineplot(x=data2.index, y=data2, palette='Set2')
-
-    # here is the trick save your figure into a bytes object and you can afterwards expose it via flas
     bytes_image = io.BytesIO()
     plt.savefig(bytes_image, format='png')
     bytes_image.seek(0)
@@ -367,13 +305,11 @@ def do_plot7():
 def do_plot8():
     # kołowy
     # Loading
-
+    global df2, data2
     plt.figure(figsize=(12, 6))
     plt.title('col 3 value count')
     plt.pie(data2, labels=data2.index, autopct='%1.1f%%', colors=colors, pctdistance=1.1, labeldistance=1.2,
             startangle=150, shadow=True)
-
-    # here is the trick save your figure into a bytes object and you can afterwards expose it via flas
     bytes_image = io.BytesIO()
     plt.savefig(bytes_image, format='png')
     bytes_image.seek(0)
@@ -385,12 +321,10 @@ def do_plot8():
 def do_plot9():
     # bar
     # Loading
-
+    global df2, data3
     plt.figure(figsize=(12, 6))
     plt.title('col 4 value count')
     sns.barplot(x=data3.index, y=data3, palette='Set2')
-
-    # here is the trick save your figure into a bytes object and you can afterwards expose it via flas
     bytes_image = io.BytesIO()
     plt.savefig(bytes_image, format='png')
     bytes_image.seek(0)
@@ -400,12 +334,10 @@ def do_plot9():
 def do_plot10():
     # line
     # Loading
-
+    global df2, data3
     plt.figure(figsize=(12, 6))
     plt.title('col 4 value count')
     sns.lineplot(x=data3.index, y=data3, palette='Set2')
-
-    # here is the trick save your figure into a bytes object and you can afterwards expose it via flas
     bytes_image = io.BytesIO()
     plt.savefig(bytes_image, format='png')
     bytes_image.seek(0)
@@ -415,13 +347,11 @@ def do_plot10():
 def do_plot11():
     # kołowy
     # Loading
-
+    global df2, data3
     plt.figure(figsize=(12, 6))
     plt.title('col 4 value count')
     plt.pie(data3, labels=data3.index, autopct='%1.1f%%', colors=colors, pctdistance=1.1, labeldistance=1.2,
             startangle=150, shadow=True)
-
-    # here is the trick save your figure into a bytes object and you can afterwards expose it via flas
     bytes_image = io.BytesIO()
     plt.savefig(bytes_image, format='png')
     bytes_image.seek(0)
@@ -433,10 +363,10 @@ def do_plot11():
 def do_plot12():
     # bar
     # Loading
+    global df2, data4
     plt.figure(figsize=(12, 6))
     plt.title('col 5 value count')
     sns.barplot(x=data4.index, y=data4, palette='Set2')
-    # here is the trick save your figure into a bytes object and you can afterwards expose it via flas
     bytes_image = io.BytesIO()
     plt.savefig(bytes_image, format='png')
     bytes_image.seek(0)
@@ -446,10 +376,10 @@ def do_plot12():
 def do_plot13():
     # line
     # Loading
+    global df2, data4
     plt.figure(figsize=(12, 6))
     plt.title('col 5 value count')
     sns.lineplot(x=data4.index, y=data4, palette='Set2')
-    # here is the trick save your figure into a bytes object and you can afterwards expose it via flas
     bytes_image = io.BytesIO()
     plt.savefig(bytes_image, format='png')
     bytes_image.seek(0)
@@ -459,13 +389,11 @@ def do_plot13():
 def do_plot14():
     # kołowy
     # Loading
-
+    global df2, data4
     plt.figure(figsize=(12, 6))
     plt.title('col 5 value count')
     plt.pie(data4, labels=data4.index, autopct='%1.1f%%', colors=colors, pctdistance=1.1, labeldistance=1.2,
             startangle=150, shadow=True)
-
-    # here is the trick save your figure into a bytes object and you can afterwards expose it via flas
     bytes_image = io.BytesIO()
     plt.savefig(bytes_image, format='png')
     bytes_image.seek(0)
@@ -477,12 +405,10 @@ def do_plot14():
 def do_plot15():
     # bar
     # Loading
-
+    global df2, data5
     plt.figure(figsize=(12, 6))
     plt.title('col 6 value count')
     sns.barplot(x=data5.index, y=data5, palette='Set2')
-
-    # here is the trick save your figure into a bytes object and you can afterwards expose it via flas
     bytes_image = io.BytesIO()
     plt.savefig(bytes_image, format='png')
     bytes_image.seek(0)
@@ -492,12 +418,10 @@ def do_plot15():
 def do_plot16():
     # line
     # Loading
-
+    global df2, data5
     plt.figure(figsize=(12, 6))
     plt.title('col 6 value count')
     sns.lineplot(x=data5.index, y=data5, palette='Set2')
-
-    # here is the trick save your figure into a bytes object and you can afterwards expose it via flas
     bytes_image = io.BytesIO()
     plt.savefig(bytes_image, format='png')
     bytes_image.seek(0)
@@ -507,13 +431,11 @@ def do_plot16():
 def do_plot17():
     # kołowy
     # Loading
-
+    global df2, data5
     plt.figure(figsize=(12, 6))
     plt.title('col 6 value count')
     plt.pie(data5, labels=data5.index, autopct='%1.1f%%', colors=colors, pctdistance=1.1, labeldistance=1.2,
             startangle=150, shadow=True)
-
-    # here is the trick save your figure into a bytes object and you can afterwards expose it via flas
     bytes_image = io.BytesIO()
     plt.savefig(bytes_image, format='png')
     bytes_image.seek(0)
@@ -522,6 +444,10 @@ def do_plot17():
 
 @app.route('/desc2/<typ>', methods=['GET'])
 def desc(typ="bar"):
+    loader = session.get('loader')
+    if not loader:
+        loader = 'csv'
+    nice_loader(loader)
     if typ == 'bar':
         bytes_obj = do_plot()
     if typ == 'pie':
@@ -530,7 +456,7 @@ def desc(typ="bar"):
         bytes_obj = do_plot1()
     # wybór do_plot albo do_plot1 albo do_plot2
     return send_file(bytes_obj,
-                     attachment_filename='plot.png',
+                     download_name='plot.png',
                      mimetype='image/png')
 
 
@@ -544,7 +470,7 @@ def desc3(typ="bar"):
         bytes_obj = do_plot4()
     # wybór do_plot albo do_plot1 albo do_plot2
     return send_file(bytes_obj,
-                     attachment_filename='plot.png',
+                     download_name='plot.png',
                      mimetype='image/png')
 
 
@@ -558,7 +484,7 @@ def desc4(typ="bar"):
         bytes_obj = do_plot7()
     # wybór do_plot albo do_plot1 albo do_plot2
     return send_file(bytes_obj,
-                     attachment_filename='plot.png',
+                     download_name='plot.png',
                      mimetype='image/png')
 
 
@@ -574,7 +500,7 @@ def desc5(typ="bar"):
         bytes_obj = do_plot10()
     # wybór do_plot albo do_plot1 albo do_plot2
     return send_file(bytes_obj,
-                     attachment_filename='plot.png',
+                     download_name='plot.png',
                      mimetype='image/png')
 
 
@@ -588,7 +514,7 @@ def desc6(typ="bar"):
         bytes_obj = do_plot13()
     # wybór do_plot albo do_plot1 albo do_plot2
     return send_file(bytes_obj,
-                     attachment_filename='plot.png',
+                     download_name='plot.png',
                      mimetype='image/png')
 
 
@@ -602,7 +528,7 @@ def desc7(typ="bar"):
         bytes_obj = do_plot16()
     # wybór do_plot albo do_plot1 albo do_plot2
     return send_file(bytes_obj,
-                     attachment_filename='plot.png',
+                     download_name='plot.png',
                      mimetype='image/png')
 
 
@@ -611,7 +537,6 @@ def desc7(typ="bar"):
 def desca():
     form = Wybieraczka(request.form)
     typ = 'bar'
-
     if request.method == 'POST' and form.validate():
         typ = form.typ.data
         print(typ)
@@ -623,7 +548,6 @@ def desca():
 def desca1():
     form = Wybieraczka(request.form)
     typ = 'bar'
-
     if request.method == 'POST' and form.validate():
         typ = form.typ.data
         print(typ)
@@ -635,7 +559,6 @@ def desca1():
 def desca2():
     form = Wybieraczka(request.form)
     typ = 'bar'
-
     if request.method == 'POST' and form.validate():
         typ = form.typ.data
         print(typ)
@@ -647,7 +570,6 @@ def desca2():
 def desca3():
     form = Wybieraczka(request.form)
     typ = 'bar'
-
     if request.method == 'POST' and form.validate():
         typ = form.typ.data
         print(typ)
@@ -659,7 +581,6 @@ def desca3():
 def desca4():
     form = Wybieraczka(request.form)
     typ = 'bar'
-
     if request.method == 'POST' and form.validate():
         typ = form.typ.data
         print(typ)
@@ -671,7 +592,6 @@ def desca4():
 def desca5():
     form = Wybieraczka(request.form)
     typ = 'bar'
-
     if request.method == 'POST' and form.validate():
         typ = form.typ.data
         print(typ)
@@ -696,16 +616,15 @@ def json_reader():
 
 
 class JsonForm(Form):
-    json = StringField('json url:', [validators. InputRequired(), validators.length(max=1000)])
+    json = StringField('json url:', [validators.InputRequired(), validators.length(max=1000)])
     send = SubmitField('send')
 
 
 @app.route('/jsonreader', methods=["GET", "POST"])
-def AQQ():
+def aqq():
     form = JsonForm(request.form)
     if request.method == 'POST':
         json = form.json.data
-        #  df = pd.read_json('data/simple.json')
         print(json)
         p = pd.read_json(json)
         print(p)
@@ -721,7 +640,7 @@ class Wybieraczka(Form):
 def plot_csv():
     return send_file('static/analiza/output.csv',
                      mimetype='text/csv',
-                     attachment_filename='output.csv',
+                     download_name='output.csv',
                      as_attachment=True)
 
 
@@ -753,29 +672,6 @@ def upload_file():
         return redirect("/")
 
 
-@app.route('/upload2')
-def upload2():
-    return render_template('upload2.html')
-
-
-@app.route('/upload2', methods=['GET', 'POST'])
-def upload_file2():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect("/")
-        flash("Wrong filename extension")
-        return redirect("/")
-
-
 @app.route('/numpy')
 def numpy_page():
     args = ""
@@ -786,6 +682,12 @@ def numpy_page():
 def pandas_page():
     args = ""
     return render_template("pandas.html", args=args)
+
+
+@app.route('/pyspark')
+def pyspark_page():
+    args = ""
+    return render_template("pyspark.html", args=args)
 
 
 @app.route('/flask')
